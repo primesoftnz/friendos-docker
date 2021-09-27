a8fbf7bbee3736cc60230b3ada4b78fac091a15c
Sasha B <abanjac84@gmail.com>
2021-09-27 14:49:37 +0700

    Added email and document server components

---
+++ docker/mariadb/6_friend_oo_interface.sql
@@ -0,0 +1,2 @@
+USE friendup;
+INSERT INTO `FSetting` (`UserID`, `Type`, `Key`, `Data`) VALUES (-1,'system','onlyoffice','{\"api_interface\":\"https://onlyoffice.myfriendup.com/web-apps/apps/api/documents/api.js\"}');
--- docker/nginx/entrypoint.sh
+++ docker/nginx/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-cp /etc/nginx/sites-available/friend.conf /etc/nginx/sites-enabled/
+ln -s /etc/nginx/sites-available/friend.conf /etc/nginx/sites-enabled/friend.conf
+cp /etc/nginx/sites-available/documentserver.conf /etc/nginx/sites-enabled/documentserver.conf
+cp /etc/nginx/sites-available/community.conf /etc/nginx/sites-enabled/community.conf
 service nginx start
 
 tail -f /var/log/nginx/access.log
